59099e225e440db68332651e381a0472a2dac993
RemondJu <remond.neiluj@gmail.com>
2018-11-14 18:03:45 +0100

    Modif App, display projet and team name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hiventive - Bee.Polar team <img src={BeePolarLogo} alt="BeePolarLogo"/></h1>
+        <h1>Projet Hiventive - Bee.Polar team <img src={BeePolarLogo} alt="BeePolarLogo"/></h1>
       </div>
     );
   }
