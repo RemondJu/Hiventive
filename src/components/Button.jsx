9959ef5aa4ac2472f1a25bd81d6ce9f8bd0576d0
RemondJu <remond.neiluj@gmail.com>
2018-11-28 11:56:21 +0100

    NavBar container, Button Component, ESlint config

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+  <button type="submit">Search</button>
+);
+
+export default Button;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 
 const allReducers = combineReducers({
-    
-})
+
+});
 
 export default allReducers;
