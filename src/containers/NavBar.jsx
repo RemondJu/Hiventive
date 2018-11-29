5d19ac5855e734bab29ed976d5204bf0df2dec0c
Mathias Moureaux <mathias.moureaux@gmail.com>
2018-11-29 16:51:53 +0100

    mise en page navbar

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './NavBar.scss';
+import logoHiventivePNG from '../images/logoHiventivePNG.png';
 
 const NavBar = () => (
   <div className="NavBar">
-    <input type="text" />
-    <button type="submit">Search</button>
-    <button type="submit">
-      Login/ Sign in
-    </button>
+    <a href="NavBar" className="logo">
+      <img className="logo" alt="logo_hiventive" src={logoHiventivePNG} />
+      <h1 className="title">Hiventive</h1>
+    </a>
+
+    <div className="searchbar">
+      <input className="field" placeholder="Search for layers" type="text" />
+      <button className="button_search" type="submit">Search</button>
+    </div>
+    <button className="button_login" type="submit">Login / Sign in</button>
   </div>
 );
 
