a6b0d6b7793f88bbfb0020d0995b083e65f5776c
fjulien <fettingerjulien@yahoo.fr>
2018-11-29 15:16:34 +0100

    config git ignore and front back

--- back/routes/conf.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: '',
-});
-module.exports = connection;
