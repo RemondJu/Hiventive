f7fe543dc81799e0e152513645b749ff3c5f063a
fjulien <fettingerjulien@yahoo.fr>
2018-12-11 16:06:43 +0100

    back. Make roads for api select all table and test

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,10 +3,38 @@ import conf from './conf';
 
 const router = express.Router();
 
-/* GET index page. */
-router.get('/', (req, res) => {
-  res.json({
-    title: 'Express'
+/* GET select all users */
+router.get('/allUsers', (req, res) => {
+  conf.query('SELECT * FROM User', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+
+/* GET select all projects */
+router.get('/allProjects', (req, res) => {
+  conf.query('SELECT * FROM Project', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+
+/* GET select all layers */
+router.get('/allLayers', (req, res) => {
+  conf.query('SELECT * FROM Layer', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
   });
 });
 
