f894cbc5c12fcbae92d0dc14165aa8453b9e24b7
Thomas Gonzalez <thomasgonzalez.pro@gmail.com>
2018-12-12 14:48:52 +0100

    info logo added, animations for app page

--- front/src/containers/LayerFromCatalog.jsx
+++ front/src/containers/LayerFromCatalog.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './LayerFromCatalog.scss';
+import info from '../images/info.png';
 
 const LayerFromCatalog = () => (
   <div className="LayerFromCatalog">
     <tr className="Layer">
-      <td className="imageRow" />
+      <td className="imageRow">  
+        <img className="info" alt="logo_info" src={info} /> 
+      </td>
       <td>Name</td>
       <td>Description</td>
       <td>Maintainer</td>
