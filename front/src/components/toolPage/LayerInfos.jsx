0863978459ea51d661f2eb0b56e42590ab34a61c
Thomas Gonzalez <thomasgonzalez.pro@gmail.com>
2018-12-12 17:06:09 +0100

    navbar modif added on App.js, added LayerPage

--- front/src/components/toolPage/LayerInfos.jsx
+++ front/src/components/toolPage/LayerInfos.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './LayerInfos.scss';
 import { NavLink } from 'react-router-dom';
-import NavBar from '../../containers/NavBar';
 
 const LayerInfos = () => (
   <div className="layerInfos">
     <NavLink to="/ToolPage">
-      <button className="button" type="submit">Back</button>
+      <button className="button" type="button">Back</button>
     </NavLink>
   </div>
 
--- front/src/containers/LayerFromCatalog.jsx
+++ front/src/containers/LayerFromCatalog.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './LayerFromCatalog.scss';
-import info from '../images/info.png';
 import { NavLink } from 'react-router-dom';
+import info from '../images/info.png';
 
 const LayerFromCatalog = () => (
   <div className="LayerFromCatalog">
     <tr className="Layer">
       <NavLink to="/LayerInfos">
         <td className="imageRow">
-          <img className="info" alt="logo_info" src={info} /> 
+          <img className="info" alt="logo_info" src={info} />
         </td>
       </NavLink>
       <td>Name</td>
