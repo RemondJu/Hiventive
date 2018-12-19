7526bbeb3172943c3f9b53055963de6ba3b24a3e
fjulien <fettingerjulien@yahoo.fr>
2018-12-19 11:10:37 +0100

    front, create button add and edit button log

--- front/src/containers/NavBar.jsx
+++ front/src/containers/NavBar.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { NavLink } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
 import './NavBar.scss';
 import '../components/ModalLogin.scss';
@@ -11,22 +12,71 @@ import switchLoginModal from '../actions';
 import ModalLogin from '../components/ModalLogin';
 import logoHiventiveWhite from '../images/logoHiventive_white.png';
 
-const NavBar = props => (
-  <div className="NavBar">
-    <NavLink to="/" className="logo">
-      <img className="logo" alt="logo_hiventive" src={logoHiventiveWhite} />
-      <h1 className="title">Hiventive</h1>
-    </NavLink>
-    <div className="searchbar">
-      <input className="field" placeholder="Search for layers" type="text" />
-      <button className="button_search" type="submit">Search</button>
-    </div>
-    <button className="button_login" type="submit" onClick={() => props.switchLoginModal()}>Login / Sign in</button>
-    <ModalLogin />
-  </div>
-);
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleAdd: false,
+      toggleLog: false,
+    };
+    this.showToggleAdd = this.showToggleAdd.bind(this);
+    this.showToggleLog = this.showToggleLog.bind(this);
+  }
+
+  showToggleAdd() {
+    this.setState(prevState => ({
+      toggleAdd: !prevState.toggleAdd,
+      toggleLog: false,
+    }));
+  }
+
+  showToggleLog() {
+    this.setState(prevState => ({
+      toggleLog: !prevState.toggleLog,
+      toggleAdd: false,
+    }));
+  }
 
 
-function mdtp(dispatch) { return bindActionCreators({ switchLoginModal }, dispatch); }
+  render() {
+    const { switchLoginModalRedux } = this.props;
+    const { toggleLog, toggleAdd } = this.state;
+    return (
+      <div className="NavBar">
+        <NavLink to="/" className="logo">
+          <img className="logo" alt="logo_hiventive" src={logoHiventiveWhite} />
+          <h1 className="title">Hiventive</h1>
+        </NavLink>
+        <div className="searchbar">
+          <input className="field" placeholder="Search for layers" type="text" />
+          <button className="button_search" type="submit">Search</button>
+        </div>
+        <div className="buttonsForPopovers">
+          <button className="button_login" onClick={() => this.showToggleAdd()} type="button">+</button>
+          <div className={toggleAdd ? 'popoversShow effectAdd' : 'popoversHid'}>
+            <div className={toggleAdd ? 'textShow' : 'textHidden'}>
+              <p>Add new Layer</p>
+            </div>
+          </div>
+          <button className="button_login" onClick={() => this.showToggleLog()} type="button">Login / Sign in</button>
+          <div className={toggleLog ? 'popoversShow effectLog' : 'popoversHid'}>
+            <div className={toggleLog ? 'textShow' : 'textHidden'}>
+              <button className="button_link_log" type="button" onClick={() => switchLoginModalRedux()}>login</button>
+            </div>
+          </div>
+        </div>
+        <ModalLogin />
+      </div>
+    );
+  }
+}
+
+NavBar.propTypes = {
+  switchLoginModalRedux: PropTypes.func.isRequired,
+};
+
+const mdtp = dispatch => bindActionCreators({
+  switchLoginModalRedux: switchLoginModal,
+}, dispatch);
 
 export default connect(null, mdtp)(NavBar);
