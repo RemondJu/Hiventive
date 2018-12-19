d6af7b34ca5bf20c95240f7edbec8ad325fca1aa
fjulien <fettingerjulien@yahoo.fr>
2018-12-19 11:39:26 +0100

    front, redux function log and add in navbar

--- front/src/containers/NavBar.jsx
+++ front/src/containers/NavBar.jsx
@@ -8,39 +8,23 @@ import PropTypes from 'prop-types';
 
 import './NavBar.scss';
 import '../components/ModalLogin.scss';
-import switchLoginModal from '../actions';
+import { showToggleAdd, showToggleLog, switchLoginModal } from '../actions';
 import ModalLogin from '../components/ModalLogin';
 import logoHiventiveWhite from '../images/logoHiventive_white.png';
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      toggleAdd: false,
-      toggleLog: false,
-    };
-    this.showToggleAdd = this.showToggleAdd.bind(this);
-    this.showToggleLog = this.showToggleLog.bind(this);
+    this.state = {};
   }
-
-  showToggleAdd() {
-    this.setState(prevState => ({
-      toggleAdd: !prevState.toggleAdd,
-      toggleLog: false,
-    }));
-  }
-
-  showToggleLog() {
-    this.setState(prevState => ({
-      toggleLog: !prevState.toggleLog,
-      toggleAdd: false,
-    }));
-  }
-
 
   render() {
-    const { switchLoginModalRedux } = this.props;
-    const { toggleLog, toggleAdd } = this.state;
+    const {
+      switchLoginModalRedux,
+      showToggleAddRedux,
+      showToggleLogRedux,
+      popoversNavbar,
+    } = this.props;
     return (
       <div className="NavBar">
         <NavLink to="/" className="logo">
@@ -52,15 +36,15 @@ class NavBar extends Component {
           <button className="button_search" type="submit">Search</button>
         </div>
         <div className="buttonsForPopovers">
-          <button className="button_login" onClick={() => this.showToggleAdd()} type="button">+</button>
-          <div className={toggleAdd ? 'popoversShow effectAdd' : 'popoversHid'}>
-            <div className={toggleAdd ? 'textShow' : 'textHidden'}>
+          <button className="button_login" onClick={() => showToggleAddRedux()} type="button">+</button>
+          <div className={popoversNavbar.toggleAdd ? 'popoversShow effectAdd' : 'popoversHid'}>
+            <div className={popoversNavbar.toggleAdd ? 'textShow' : 'textHidden'}>
               <p>Add new Layer</p>
             </div>
           </div>
-          <button className="button_login" onClick={() => this.showToggleLog()} type="button">Login / Sign in</button>
-          <div className={toggleLog ? 'popoversShow effectLog' : 'popoversHid'}>
-            <div className={toggleLog ? 'textShow' : 'textHidden'}>
+          <button className="button_login" onClick={() => showToggleLogRedux()} type="button">Login / Sign in</button>
+          <div className={popoversNavbar.toggleLog ? 'popoversShow effectLog' : 'popoversHid'}>
+            <div className={popoversNavbar.toggleLog ? 'textShow' : 'textHidden'}>
               <button className="button_link_log" type="button" onClick={() => switchLoginModalRedux()}>login</button>
             </div>
           </div>
@@ -73,10 +57,19 @@ class NavBar extends Component {
 
 NavBar.propTypes = {
   switchLoginModalRedux: PropTypes.func.isRequired,
+  showToggleAddRedux: PropTypes.func.isRequired,
+  showToggleLogRedux: PropTypes.func.isRequired,
+  popoversNavbar: PropTypes.shape.isRequired,
 };
+
+const mstp = state => ({
+  popoversNavbar: state.popoversNavbar,
+});
 
 const mdtp = dispatch => bindActionCreators({
   switchLoginModalRedux: switchLoginModal,
+  showToggleAddRedux: showToggleAdd,
+  showToggleLogRedux: showToggleLog,
 }, dispatch);
 
-export default connect(null, mdtp)(NavBar);
+export default connect(mstp, mdtp)(NavBar);
---
+++ front/src/reducers/popoversNavbar.js
@@ -0,0 +1,21 @@
+const initialState = {
+  toggleAdd: false,
+  toggleLog: false,
+};
+
+const popoversNavbar = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SWITCH_LOGIN_MODAL':
+      return { ...state, toggleAdd: false, toggleLog: false };
+
+    case 'SHOW_TOGGLE_ADD':
+      return { ...state, toggleAdd: !state.toggleAdd, toggleLog: false };
+
+    case 'SHOW_TOGGLE_LOG':
+      return { ...state, toggleLog: !state.toggleLog, toggleAdd: false };
+
+    default: return state;
+  }
+};
+
+export default popoversNavbar;
