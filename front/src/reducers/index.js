3d22e2674faa956729b7754e6195592a2c3a021d
fjulien <fettingerjulien@yahoo.fr>
2019-01-03 11:13:30 +0100

    front, create page loading

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import modal from './modal';
 import popoversNavbar from './popoversNavbar';
 import categoryLayer from './categoryLayer';
+import isLoading from './isLoading';
 
 const allReducers = combineReducers({
   modal,
   popoversNavbar,
   categoryLayer,
+  isLoading,
 });
 
 export default allReducers;
