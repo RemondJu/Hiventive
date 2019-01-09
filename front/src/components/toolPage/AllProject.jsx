5cc1aa008068d891c3430cfb8fbd07b4a1658e44
Agnes Ribeiro <mu21@hotmail.fr>
2019-01-09 17:00:35 +0100

    fixed projectPage bug

--- front/src/components/toolPage/AllProject.jsx
+++ front/src/components/toolPage/AllProject.jsx
@@ -3,8 +3,8 @@ import ProjectSideBar from '../../containers/ProjectSideBar';
 import './ToolPage.scss';
 
 const AllProject = () => (
-  <div className="ToolPage">
-    <div className="toolRow">
+  <div className="AllProject">
+    <div className="projectRow">
       <ProjectSideBar />
     </div>
   </div>
