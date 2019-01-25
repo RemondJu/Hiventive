879db2c164dfc8163a8b0425a32dd574872f15ab
RemondJu <remond.neiluj@gmail.com>
2019-01-25 10:16:58 +0100

    fixes on layers dispay, sidebar and titles

--- front/src/containers/PageProject.jsx
+++ front/src/containers/PageProject.jsx
@@ -165,6 +165,9 @@ class PageProject extends Component {
         </div>
         <div className="titleProject">
           <div className="titleDisplay">
+            <div className="flexAdjust">
+              {''}
+            </div>
             <form className="titleDesc" onSubmit={this.sendProjectUpdate}>
               {editionButtonEnabled ? (
                 <label className="label_input" htmlFor="newProjectName">
