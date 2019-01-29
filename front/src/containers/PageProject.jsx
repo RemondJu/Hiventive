0447bccaf08abe6d997098e6393c5ee00582326e
fjulien <fettingerjulien@yahoo.fr>
2019-01-29 15:37:37 +0100

    changed build project page bis

--- front/src/containers/PageProject.jsx
+++ front/src/containers/PageProject.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './PageProject.scss';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
